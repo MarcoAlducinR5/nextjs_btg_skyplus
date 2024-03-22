f9684ce558142007743e1ba91663fea70e877764
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-03-22 11:30:01 -0600

    Navegación y mock del home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <>
-      <p>home &#40;&#47;&#41; &#61;&#62; Bienvenida</p>
+      <p style={{color:'blue', fontSize: '28px', fontWeight: '400' }}>Bienvenido, recuerda que</p>
+      <p style={{color:'blue', fontSize: '28px', fontWeight: 'bolder' }}>Blue To Go Sports ahora es Sky+</p>
+
+      <p style={{color:'gray', fontSize: '16px', fontWeight: '600' }}>Podrás ver completamente en VIVO las ligas deportivas más importantes del mundo, estés donde estés. Sólo para MX .</p>
+
+      <div>
+
+        <Link href="/ingresa">
+          <button style={{marginRight: '40px'}}>Iniciar sesión</button> 
+        </Link>
+
+        <Link href="/validarEmail">
+          <button>Quiero contratar</button>
+        </Link>
+      </div>
     </>
   );
 }
