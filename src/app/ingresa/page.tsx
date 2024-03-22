6715a52f9bb025c7fc87b4975ed72a8b5c26f1cc
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-03-22 17:18:10 -0600

    se hacen ajustes de formulario

--- src/app/ingresa/page.tsx
+++ src/app/ingresa/page.tsx
@@ -1,3 +1,27 @@
+
+import './ingresa.css'
+import Link from 'next/link';
+
 export default function Page() {
-    return <p>Iniciar sesion</p>;
+    return (<>
+      
+      <p style={{color:'blue', fontSize: '28px', fontWeight: 'bolder' }}>Blue To Go Sports ahora es Sky+</p>
+
+      <p style={{color:'gray', fontSize: '16px', fontWeight: '600' }}>Introduce los datos de tu cuenta BTG Sports</p>
+
+      <form >
+            <label>Usuario</label>
+            <input placeholder="Usuario" value={"text"}/>
+            <label>Password</label>
+            <input placeholder="Contraseña" 
+            value={"text"}/>
+            <div className="TodoForm-buttonContainer">
+                <button 
+                    type="submit"
+                    className="TodoForm-button TodoForm-button--add">
+                        Iniciar sesión
+                </button>
+            </div>
+    </form>
+    </>);
 }
